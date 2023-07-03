e59394bab9bbc8db8bdb9e0e9b3aa39ca0561aba
Valentine1244 <akamigboifeanyi22@gmail.com>
2023-07-03 11:04:37 -0700

    empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a module containing the class for a rectangle.
+
+It contains nothing except pass
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class containing nothing but the pass
+    command to enable it throw no error
+    """
+    pass
