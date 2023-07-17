6183a34fde71c1b9e43336c19792a84fa221aa19
Valentine1244 <akamigboifeanyi22@gmail.com>
2023-07-17 13:37:12 -0700

    updated modules

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,79 +1,136 @@
 #!/usr/bin/python3
+"""Provides a base class for all other classes in this module
+"""
+
 import json
-
-"""
-This is a file for the base class
-"""
+import turtle
 
 
-class Base:
+class Base():
+    """Base class for all other classes in this module
     """
-    This is the base class for the project
-    """
+    HEADERS = ('id',)
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None:
-            self.id = id
+        """Instantiate a base object
+        """
+        if id is None:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = id
 
     @staticmethod
-    def to_json_string(list_dictionaries):
+    def draw(list_rectangles, list_squares):
+        """Draw rectangles and squares in a new window
         """
-        converts Json object to the str representation
-        """
-        if type(list_dictionaries) is list and list_dictionaries is not None:
-            return json.dumps(list_dictionaries)
-        else:
-            return json.dumps([])
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """
-        Saves the list_objs to file
-        """
-        if type(list_objs) is list:
-            with open(cls.__name__+".json", mode="w") as newfile:
-                newfile.write(cls.to_json_string(
-                    [x.__getattribute__("to_dictionary")()
-                        for x in list_objs if type(x) is cls]))
+        shapes = []
+        if list_rectangles:
+            shapes.extend(list_rectangles)
+        if list_squares:
+            shapes.extend(list_squares)
+        pen = turtle.Turtle()
+        pen.pen(pencolor='black', pendown=False, pensize=2, shown=False)
+        for shape in shapes:
+            pen.penup()
+            pen.setpos(shape.x, shape.y)
+            pen.pendown()
+            pen.forward(shape.width)
+            pen.right(90)
+            pen.forward(shape.height)
+            pen.right(90)
+            pen.forward(shape.width)
+            pen.right(90)
+            pen.forward(shape.height)
+            pen.right(90)
 
     @staticmethod
     def from_json_string(json_string):
+        """Return the list defined by a JSON string
         """
-        Converts json String to Json Object
-        """
-        if type(json_string) is str and json_string is not None:
-            return json.loads(json_string)
-        else:
+        if json_string is None:
             return []
+        return json.loads(json_string)
 
-    @classmethod
-    def create(cls, **dictionary):
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return a JSON representation a list of dictionaries
         """
-        Creates a new instance
-        """
-        if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummy = cls(1)
-        else:
-            dummy = None
-        dummy.update(**dictionary)
-        return dummy
+        if list_dictionaries is None:
+            return '[]'
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def load_from_file(cls):
+        """Load the objects defined in the JSON file <class-name>.json
         """
-        Loads json from file
-        """
-        filename = cls.__name__ + ".json"
         try:
-            with open(filename, 'r') as file:
-                json_data = file.read()
-                dict_list = cls.from_json_string(json_data)
-                return [cls.create(**data) for data in dict_list]
+            with open("{}.json".format(cls.__name__), 'r') as ifile:
+                return [cls.create(**obj)
+                        for obj in cls.from_json_string(ifile.read())]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Save a CSV representation of list_objs to <class_name>.json
+        """
+        try:
+            with open("{}.csv".format(cls.__name__), 'r') as ifile:
+                return [cls.create(
+                    **{k: int(v) for k, v in zip(cls.HEADERS, line.split(','))}
+                ) for line in ifile.readlines()]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save a JSON representation of list_objs to <class-name>.json
+        """
+        with open("{}.json".format(cls.__name__), 'w') as ofile:
+            if list_objs:
+                ofile.write(cls.to_json_string(
+                    [obj.to_dictionary() for obj in list_objs]
+                ))
+            else:
+                ofile.write("[]")
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Save a CSV representation of list_objs to <class-name>.json
+        """
+        with open("{}.csv".format(cls.__name__), 'w') as ofile:
+            if list_objs:
+                for obj in list_objs:
+                    obj = obj.to_dictionary()
+                    ofile.write(
+                        ','.join(str(obj[key]) for key in cls.HEADERS) + '\n'
+                    )
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a new instance of cls with its attributes set
+        """
+        args = []
+        while True:
+            try:
+                obj = cls(*args)
+            except TypeError:
+                args.append(1)
+            else:
+                break
+        obj.update(**dictionary)
+        return obj
+
+    def update(self, *args, **kwargs):
+        """Update the attributes of a base object
+        """
+        if args:
+            for pair in zip(self.HEADERS, args):
+                setattr(self, *pair)
+        else:
+            for key in kwargs:
+                if key in self.HEADERS:
+                    setattr(self, key, kwargs[key])
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,133 +1,123 @@
 #!/usr/bin/python3
+"""Provides a class to represent a rectangle
+"""
+
 from models.base import Base
 
 
-"""
-This file contains the class for the rectangle class
-"""
-
-
 class Rectangle(Base):
+    """Representation of a rectangle
     """
-    This is the rectangle class which inherits from the base class
-    """
+    HEADERS = ('id', 'width', 'height', 'x', 'y')
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        elif width <= 0:
-            raise ValueError("width width must be > 0")
-        elif type(height) is not int:
-            raise TypeError("height must be an integer")
-        elif height <= 0:
-            raise ValueError("height width must be > 0")
-        elif type(x) is not int:
-            raise TypeError("x must be an integer")
-        elif x < 0:
-            raise ValueError("x must be >= 0")
-        elif type(y) is not int:
-            raise TypeError("y must be an integer")
-        elif y < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.width = width
-            self.height = height
-            self.x = x
-            self.y = y
-            super().__init__(id)
+        """Instantiate a rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} \
-- {}/{}".format(self.id, self.__x, self.__y,
-                self.__width, self.__height)
+        """Get a string representation of a rectangle
+        """
+        return "[{type}] ({id}) {x}/{y} - {width}/{height}".format(
+            type=self.__class__.__name__,
+            id=self.id,
+            width=self.__width,
+            height=self.__height,
+            x=self.__x,
+            y=self.__y
+        )
 
     @property
     def width(self):
+        """Get private instance attribute 'width'
+        """
         return self.__width
 
     @width.setter
-    def width(self, value):
-        if type(value) is not int:
+    def width(self, width):
+        """Set private instance attribute 'width'
+        """
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width width must be > 0")
-        else:
-            self.__width = value
+        if width < 1:
+            raise ValueError("width must be > 0")
+        self.__width = width
 
     @property
     def height(self):
+        """Get private instance attribute 'height'
+        """
         return self.__height
 
     @height.setter
-    def height(self, value):
-        if type(value) is not int:
+    def height(self, height):
+        """Set private instance attribute 'height'
+        """
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height width must be > 0")
-        self.__height = value
+        if height < 1:
+            raise ValueError("height must be > 0")
+        self.__height = height
 
     @property
     def x(self):
+        """Get private instance attribute 'x'
+        """
         return self.__x
 
     @x.setter
-    def x(self, value):
-        if type(value) is not int:
+    def x(self, x):
+        """Set private instance attribute 'x'
+        """
+        if not isinstance(x, int):
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if x < 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.__x = x
 
     @property
     def y(self):
+        """Get private instance attribute 'y'
+        """
         return self.__y
 
     @y.setter
-    def y(self, value):
-        if type(value) is not int:
+    def y(self, y):
+        """Set private instance attribute 'y'
+        """
+        if not isinstance(y, int):
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if y < 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+        self.__y = y
 
     def area(self):
+        """Return the area of a rectangle
         """
-        Finds the area
-        """
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def display(self):
+        """Print a text representation of a rectangle
         """
-        Prints the visual of the object
-        """
-        for space in range(self.__y):
-            print()
-        for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
-
-    def update(self, *args, **kwargs):
-        """
-        Updates the value of the rectangle
-        """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
-        if len(args) == 0 and len(kwargs) > 0:
-            for key, val in kwargs.items():
-                setattr(self, key, val)
+        print("\n" * self.__y, end="")
+        print("\n".join([" " * self.__x + "#" * self.__width] * self.__height))
 
     def to_dictionary(self):
+        """Get a dictionary representation of a rectangle
         """
-        Converts the object to a dictinoary
+        return {key: getattr(self, key) for key in self.__class__.HEADERS}
+
+    def update(self, *args, **kwargs):
+        """Update the attributes of a  object
         """
-        return {"x": self.x, "y": self.y,
-                "id": self.id, "height": self.height, "width": self.width}
+        if args:
+            for pair in zip(self.HEADERS, args):
+                setattr(self, *pair)
+        else:
+            for key in kwargs:
+                if key in self.HEADERS:
+                    setattr(self, key, kwargs[key])
