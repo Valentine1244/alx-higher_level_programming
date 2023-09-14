ce1e72cf2cb452fb11a8c1953e0f4424946c6ed8
Akamigbo <akamigboifeanyi22@gmail.com>
2023-09-14 07:28:16 -0700

    select state

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Reading a database table"""
+
+if __name__ == '__main__':
+        import MySQLdb
+            import sys
+
+                if (len(sys.argv) != 4):
+                            quit()
+                                db = MySQLdb.connect(host='localhost',
+                                                                 port=3306,
+                                                                                          user=sys.argv[1],
+                                                                                                                   password=sys.argv[2],
+                                                                                                                                            db=sys.argv[3])
+                                    cur = db.cursor()
+                                        r = cur.execute('SELECT * FROM states')
+                                            rows = cur.fetchall()
+                                                for row in rows:
+                                                            print(row)
+                                                                cur.close()
+                                                                    db.close()
