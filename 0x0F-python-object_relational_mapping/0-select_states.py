b642345c78d5ea1a754356392bf28c1dd29bc660
Akamigbo <akamigboifeanyi22@gmail.com>
2023-09-14 07:45:58 -0700

    correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,20 +2,20 @@
 """Reading a database table"""
 
 if __name__ == '__main__':
-        import MySQLdb
-            import sys
+    import MySQLdb
+    import sys
 
-                if (len(sys.argv) != 4):
-                            quit()
-                                db = MySQLdb.connect(host='localhost',
-                                                                 port=3306,
-                                                                                          user=sys.argv[1],
-                                                                                                                   password=sys.argv[2],
-                                                                                                                                            db=sys.argv[3])
-                                    cur = db.cursor()
-                                        r = cur.execute('SELECT * FROM states')
-                                            rows = cur.fetchall()
-                                                for row in rows:
-                                                            print(row)
-                                                                cur.close()
-                                                                    db.close()
+    if (len(sys.argv) != 4):
+        quit()
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         password=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    r = cur.execute('SELECT * FROM states')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
